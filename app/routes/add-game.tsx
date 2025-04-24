6fad22df053a721eee6c63b809714c382682c6d3
FranDaMan <1343045@hc.ac.je>
2025-04-24 12:16:36 +0100

    added localhost-add game to webpage, and chenged title

---
+++ app/routes/add-game.tsx
@@ -0,0 +1,5 @@
+export default function AddGame() {
+
+return  <div className="container mx-auto py-20 text-9xl font-bold animation- "> <h1>Add <span className="text-cyan-400"> Game </span></h1></div>
+
+}
