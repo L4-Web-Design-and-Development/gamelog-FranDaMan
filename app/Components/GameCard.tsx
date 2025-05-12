a31c0eaccc6b0a26d5c252e64fa38680c4f5e228
FranDaMan <1343045@hc.ac.je>
2025-05-12 12:02:08 +0100

    gamelog layout fully sized up

--- app/Components/GameCard.tsx
+++ app/Components/GameCard.tsx
@@ -12,28 +12,32 @@ export default function GameCard({
 }: GameCardProps) {
   const formattedDate = releaseDate.slice(0, 10);
   return (
-    <div className=" p-3">
-      <div className="flex justify-center items-center w-80  rounded-2xl">
+    <div className="m-3">
+      <div className="flex justify-center items-center w-96  rounded-2xl">
         <img
-          className="rounded-2xl h-48"
+          className="rounded-2xl h-56"
           src={imageURL}
           alt={`${title} cover`}
         />
       </div>
       <div>
         <div className="flex justify-between items-center my-5">
-          <div className="h-24 flex flex-col justify-between">
-            <div className="flex items-center font-bold w-48 h-7">{title}</div>
-            <div className="text-cyan-300 font-semibold items-center">
+          <div className="h-28 flex flex-col justify-between">
+            <div className="flex items-center font-bold w-56 h-9 text-lg">
+              {title}
+            </div>
+            <div className="text-cyan-300 font-semibold items-center text-lg">
               {categoryTitle}
             </div>
-            <div className="text-zinc-500 items-center">{formattedDate}</div>
+            <div className="text-zinc-500 items-center text-lg">
+              {formattedDate}
+            </div>
           </div>
           <div className="flex flex-col justify-between gap-10">
-            <div className="flex justify-center items-center border-2 border-cyan-300 text-cyan-300 w-24 rounded-md h-7 text-sm">
+            <div className="flex justify-center items-center border-2 border-cyan-300 text-cyan-300 w-28 rounded-md h-8 text-base">
               Edit
             </div>
-            <div className="flex justify-center items-center border-2 border-red-300 text-red-300 w-24 rounded-md h-7 text-sm">
+            <div className="flex justify-center items-center border-2 border-red-300 text-red-300 w-28 rounded-md h-8 text-base">
               Delete
             </div>
           </div>
