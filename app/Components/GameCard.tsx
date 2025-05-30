3f362347d68a61fd94fa6b57f91748e27e1ee8bb
FranDaMan <1343045@hc.ac.je>
2025-05-30 14:32:21 +0100

    Update GameCard.tsx

--- app/Components/GameCard.tsx
+++ app/Components/GameCard.tsx
@@ -18,10 +18,10 @@ export default function GameCard({
   const formattedDate = releaseDate.slice(0, 10);
 
   return (
-    <div className="px-4 pb-4 ">
+    <div className="px-4 pb-9 ">
       <Link
         to={`/edit-game/${id}`}
-        className="relative flex justify-center h-72 items-center w-auto rounded-2xl overflow-hidden lg:h-52"
+        className="relative flex justify-center h-72 items-center w-auto rounded-2xl overflow-hidden lg:h-60"
       >
         {" "}
         <img
@@ -32,12 +32,12 @@ export default function GameCard({
       </Link>
 
       <div>
-        <div className="  relative flex justify-between items-center mx-auto mt-4">
-          <div className="h-28 flex flex-col justify-between">
+        <div className="  relative flex justify-between items-center mx-auto mt-5 md:mt-8 lg:mt-4">
+          <div className="lg:h-20 md:h-32 h-28 flex flex-col justify-between">
             <div className="flex">
               <div
                 title={title}
-                className="whitespace-nowrap overflow-hidden text-ellipsis md:truncate w-full md:w-40 lg:w-20 xl:w-40 font-bold text-base cursor-default select-none"
+                className="whitespace-nowrap overflow-hidden text-ellipsis md:truncate w-full md:w-40 lg:w-28 xl:w-40 2xl:w-52  font-bold text-base cursor-default select-none"
               >
                 {title}
               </div>
@@ -50,16 +50,16 @@ export default function GameCard({
               {formattedDate}
             </div>
           </div>
-          <div className="flex flex-col justify-between items-center gap-10">
+          <div className="flex flex-col justify-between items-center md:gap-14 lg:gap-7 gap-7">
             <Link to={`/edit-game/${id}`}>
-              <button className="flex justify-center w-32 h-12 items-center border-2 border-cyan-300 text-cyan-300 lg:w-28 rounded-md lg:h-8  text-base hover:bg-cyan-300/30 transition">
+              <button className="flex justify-center w-32 h-8 items-center border-2 border-cyan-300 text-cyan-300 lg:w-24 rounded-sm lg:h-6 lg:text-xs text-base md:w-28 hover:bg-cyan-300/30 transition">
                 Edit
               </button>
             </Link>
             <Form action={`/delete-game/${id}`} method="post">
               <button
                 type="submit"
-                className="flex justify-center w-32 h-12 items-center border-2 border-red-300 text-red-300 lg:w-28 rounded-md lg:h-8 text-base hover:bg-red-300/30 transition"
+                className="flex justify-center w-32 h-8 items-center border-2 border-red-300 text-red-300 lg:w-24 rounded-sm lg:h-6 text-base md:w-28 lg:text-xs hover:bg-red-300/30 transition"
               >
                 Delete
               </button>
